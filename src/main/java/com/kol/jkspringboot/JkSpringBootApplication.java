9466ce1c906dec0fff17896bedc96660c8b847f3
wjt188 <78445294+wjt188@users.noreply.github.com>
2023-12-09 20:00:39 +0800

    Initial commit

---
+++ src/main/java/com/kol/jkspringboot/JkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kol.jkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JkSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kol/jkspringboot/JkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kol.jkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
